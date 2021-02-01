ce85126f081cf36add13cdbd99f5cc691de07c6c
thewalkingduff <62387646+thewalkingduff@users.noreply.github.com>
2021-01-31 21:53:42 -0500

    updated text

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ejs = require("ejs");
 
 const app = express()
 
-const aboutContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo nec ultrices dui sapien eget mi proin sed. Vitae ultricies leo integer malesuada nunc vel risus. At augue eget arcu dictum. Iaculis at erat pellentesque adipiscing commodo elit. Eget felis eget nunc lobortis mattis aliquam faucibus purus. Pretium viverra suspendisse potenti nullam ac tortor.'
+const aboutContent = "Based just north of Dallas, JC Transport's offers unmatched service at competitive rates to lanes all over the Texarkana region. We take pride in our ability to provide timely and reliable service, with our promise of satisfaction delivered with each shipment we move. Our dedicated staff works closely with all of our customers to maintain the highest levels of communication and support.  We invest greatly in all services provided, and no project is ever too big or small. Our primary goal is to always provide quality services to our customers."
 
 
 app.set('view engine', 'ejs')
