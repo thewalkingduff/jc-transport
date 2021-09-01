a98ebfe93c8ebc3ab5eb4ec8ef0afffc4ee97826
Brendan Duffy <bduffy@devduffy.com>
2021-09-01 16:22:43 -0400

    cleaned up EJS a little

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require("express");
-
+const app = express()
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 8000;
   }
-
-const ejs = require("ejs");
-
-const app = express()
-
-const aboutContent = "Based just north of Dallas, JC Transport's offers unmatched service at competitive rates to lanes all over the Texarkana region. We take pride in our ability to provide timely and reliable service, with our promise of satisfaction delivered with each shipment we move. Our dedicated staff works closely with all of our customers to maintain the highest levels of communication and support.  We invest greatly in all services provided, and no project is ever too big or small. Our primary goal is to always provide quality services to our customers."
-
 
 app.set('view engine', 'ejs')
 
@@ -21,6 +14,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) =>{
     res.render('home')  
 })
+
+const aboutContent = "Based just north of Dallas, JC Transport's offers unmatched service at competitive rates to lanes all over the Texarkana region. We take pride in our ability to provide timely and reliable service, with our promise of satisfaction delivered with each shipment we move. Our dedicated staff works closely with all of our customers to maintain the highest levels of communication and support.  We invest greatly in all services provided, and no project is ever too big or small. Our primary goal is to always provide quality services to our customers."
 
 app.get('/about', (req, res) =>{
     res.render('about', {
